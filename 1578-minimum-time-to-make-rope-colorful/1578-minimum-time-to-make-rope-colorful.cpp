53c1deeac28b6d9254302505f4c6c41556750b70
Koushik <109598073+Kou-shik2004@users.noreply.github.com>
2023-12-27 11:52:11 +0530

    Time: 119 ms (58.12%), Space: 95.7 MB (83.31%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    int minCost(string colors, vector<int>& neededTime) {
+     int minCost(string colors, vector<int>& neededTime) {
         int total=0;
         int left=0;
         for(int right=1;right<colors.length();right++){
             if(colors[left]==colors[right]){
                 if(neededTime[left]<neededTime[right]){
                     total+=neededTime[left];
-                    left=right;
+                    left=right; 
                 }
                 else{
                     total+=neededTime[right];
+                    //the left pointer stays on the same position
                 }
             }
             else{
@@ -19,4 +20,22 @@ public:
         }
         return total;
     }
+
+    // int minCost(string s, vector<int>& cost) 
+    // {
+    //     int res = 0;
+    //     for(int i = 1; i < s.size(); i++)
+    //     {
+    //         if(s[i] == s[i - 1])
+    //         {
+    //             res += min(cost[i], cost[i - 1]);
+    //             cost[i] = max(cost[i], cost[i - 1]); 
+
+    //         }     
+    //     }
+        
+    //     return res;
+        
+    // }
+   
 };
