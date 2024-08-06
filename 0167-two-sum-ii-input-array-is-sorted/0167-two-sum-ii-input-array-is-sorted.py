56055a907b151e875773bd2502eed3b451f03d11
Koushik <skoushik20040804@gmail.com>
2024-08-06 07:21:37 +0530

    Time: 107 ms (43.33%), Space: 17.8 MB (24.03%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,8 +1,16 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-       map={} # value : index
-       for index,num in enumerate(numbers):
-            if target-num in map:
-                return [map[target-num]+1,index+1]
-            map[num]=index
+       #logic compare first and last number if f+l>target
+       #do l-- else f++ until f<=l
+       first=0
+       last=len(numbers)-1
+       while first<=last:
+            if numbers[first]+numbers[last]==target:
+                return [first+1,last+1]
+            elif numbers[first]+numbers[last]<target:
+                first+=1
+                continue
+            else:
+                last-=1
+                continue
        return
